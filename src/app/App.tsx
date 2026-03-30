4cacf9519fa95206791d96b7c3e879124009fce6
PJongHo03 <ghdls1254@gmail.com>
2026-03-30 17:55:52 +0900

    feat: testAPI 연동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,21 @@
 import Router from "../routes";
 import GlobalStyle from "../styles/globalStyle";
 
+import { testApiConnection } from "../services/testAPI";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const testConnection = async () => {
+      try {
+        const data = await testApiConnection();
+        console.log("API Connection Successful:", data);
+      } catch (error) {
+        console.error("API Connection Failed:", error);
+      }
+    };
+    testConnection();
+  }, []);
   return (
     <>
       <GlobalStyle />
--- src/services/testAPI.ts
+++ src/services/testAPI.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "./axiosInstance";
 
 export const testApiConnection = async () => {
-  const response = await axiosInstance.get("/health");
+  const response = await axiosInstance.get("/test");
   return response.data;
 };
