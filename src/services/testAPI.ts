fcc882cd4e46b3dcf28182654bd3c0b0a81c5f51
PJongHo03 <ghdls1254@gmail.com>
2026-04-08 09:54:51 +0900

    refactor: api수정

--- src/services/testAPI.ts
+++ src/services/testAPI.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "./axiosInstance";
 
 export const testApiConnection = async () => {
-  const response = await axiosInstance.get("/test");
+  const response = await axiosInstance.get("/api/club-intro");
   return response.data;
 };
