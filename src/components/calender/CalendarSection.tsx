98668ce34a1fcb625bce82c5ba956d1307d03fce
chohangill <james506202@gmail.com>
2026-04-13 18:56:51 +0900

    캘린더 수정

--- src/components/calender/CalendarSection.tsx
+++ src/components/calender/CalendarSection.tsx
@@ -1,18 +1,19 @@
 import * as S from "./CalendarSection.styles";
-import BlurGlow from "../common/blur/BlurGlow";
-import heroImage from "../../assets/images/calendar-bg.jpg";
+import heroImage from "../../assets/images/calendar-bg.png";
 
 type CalendarDay = {
   day: number | null;
-  event?: string;
-  eventColor?: "blue" | "white";
+  holiday?: string;
+  clubEvent?: string;
+  holidayColor?: "blue" | "white";
+  clubEventColor?: "blue" | "white";
 };
 
 const CALENDAR_WEEKS: CalendarDay[][] = [
   [
-    { day: 1, eventColor: "blue" },
-    { day: 2, event: "리뉴얼", eventColor: "blue" },
-    { day: 3, event: "개강", eventColor: "white" },
+    { day: 1, holiday: "3.1절", holidayColor: "blue" },
+    { day: 2, holiday: "대체공휴일", holidayColor: "blue" },
+    { day: 3, clubEvent: "개강", clubEventColor: "white" },
     { day: 4 },
     { day: 5 },
     { day: 6 },
@@ -31,8 +32,8 @@ const CALENDAR_WEEKS: CalendarDay[][] = [
     { day: 15 },
     { day: 16 },
     { day: 17 },
-    { day: 18 },
-    { day: 19 },
+    { day: 18, clubEvent: "임원 대면 회의", clubEventColor: "white" },
+    { day: 19, clubEvent: "동아리 OT", clubEventColor: "white" },
     { day: 20 },
     { day: 21 },
   ],
@@ -56,38 +57,33 @@ const CALENDAR_WEEKS: CalendarDay[][] = [
   ],
 ];
 
-const EVENT_SUMMARY = ["3.1 삼일절", "3.2 개강"];
+const EVENT_SUMMARY = [
+  "3.1 삼일절",
+  "3.2 대체공휴일",
+  "3.3 개강",
+  "3.18 임원 대면 회의",
+  "3.19 동아리 OT",
+];
 
 export default function CalendarSection() {
   return (
     <S.Wrapper>
       <S.Hero $image={heroImage}>
+        <S.HeroOverlay />
         <S.HeroContent>
           <S.HeroTitle>REVERSE 일정</S.HeroTitle>
           <S.HeroDesc>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique.
+            {"\n"}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique.
           </S.HeroDesc>
         </S.HeroContent>
+        <S.HeroLine />
       </S.Hero>
 
       <S.Section>
-        <BlurGlow
-          color='#5580EF'
-          blur={100}
-          size={320}
-          right='28vw'
-          bottom='7vw'
-          opacity={0.5}
-        />
-        <BlurGlow
-          color='#6A3EF0'
-          blur={100}
-          size={300}
-          left='30vw'
-          top='6vw'
-          opacity={0.35}
-        />
         <S.Content>
           <S.YearText>2026</S.YearText>
           <S.MonthText>3월</S.MonthText>
@@ -96,27 +92,35 @@ export default function CalendarSection() {
             <S.Grid>
               {CALENDAR_WEEKS.flat().map((item, index) => (
                 <S.Cell key={`${item.day}-${index}`}>
-                  {item.day && (
-                    <S.DayNumber $variant={item.eventColor}>
+                  {item.day !== null && (
+                    <S.DayNumber
+                      $variant={item.holidayColor || item.clubEventColor}
+                    >
                       {item.day}
                     </S.DayNumber>
                   )}
 
-                  {item.event && (
-                    <S.EventText $variant={item.eventColor ?? "white"}>
-                      {item.event}
+                  {item.holiday && (
+                    <S.HolidayText $variant={item.holidayColor ?? "blue"}>
+                      {item.holiday}
+                    </S.HolidayText>
+                  )}
+
+                  {item.clubEvent && (
+                    <S.EventText $variant={item.clubEventColor ?? "white"}>
+                      {item.clubEvent}
                     </S.EventText>
                   )}
                 </S.Cell>
               ))}
             </S.Grid>
-
-            <S.EventSummary>
-              {EVENT_SUMMARY.map((event) => (
-                <S.EventSummaryItem key={event}>• {event}</S.EventSummaryItem>
-              ))}
-            </S.EventSummary>
           </S.CalendarBox>
+
+          <S.EventSummaryBox>
+            {EVENT_SUMMARY.map((event) => (
+              <S.EventSummaryItem key={event}>• {event}</S.EventSummaryItem>
+            ))}
+          </S.EventSummaryBox>
         </S.Content>
       </S.Section>
     </S.Wrapper>
